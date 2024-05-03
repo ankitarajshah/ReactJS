31d774b887b7da1644ae240e2b778d536c3759af
Ankitarajshah <k_anku@yahoo.co.in>
2024-05-03 14:02:40 +0530

    addpages

---
+++ React-App-Practise/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Button } from "@/components/ui/button";
+
+const Header = () => {
+  return (
+    <>
+      <h1>Login App</h1>
+      <div>{/* <Button>Click me</Button> */}</div>
+    </>
+  );
+};
+export default Header;
---
+++ React-App-Practise/src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <main></main>
+    </>
+  );
+};
+export default Login;
